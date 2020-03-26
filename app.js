937b0cb8818a0262ab411a4a323463e0da0a6b9f
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-03-26 10:00:15 -0400

    transferred files

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+app.use(morgan('common'));
+app.use(cors());
+
+const store = require('./store.js');
+
+app.get('/apps', (req, res) => {
+    const { search='', sort, genre} = req.query;
+
+    if (sort) {
+        if (!['Rating', 'App'].includes(sort)) {
+            return res.status(400)
+                .send('Sort must be one of Rating or App');
+        }
+    }
+
+    if (genre) {
+        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
+            return res.status(400)
+                .send('Genre can be Action, Puzzle, Strategy, Casual, Arcade, or Card.')
+        }
+    }
+
+    let results = store.filter(option => 
+        option.App.toLowerCase()
+        .includes(search.toLowerCase()));
+
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        });
+    }
+
+    if (genre) {
+        results = results.filter(option => 
+            option.Genres.includes(genre));
+    }
+
+    res
+        .json(results);
+})
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+})
