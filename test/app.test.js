74faeefbb3980d9a0c02ce6edad1dfe3ff11455c
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-03-31 11:32:54 -0400

    fixed testing for wrong parameter

--- test/app.test.js
+++ test/app.test.js
@@ -62,7 +62,7 @@ describe('GET /apps', () => {
     it('should only sort by Rating and App', () => {
         return supertest(app)
             .get('/apps')
-            .query(!['Rating', 'App'])
+            .query({sort: 'other'})
             .expect(400, 'Sort must be one of Rating or App.')
     })
     //sort works with correct parameters
@@ -74,14 +74,75 @@ describe('GET /apps', () => {
             .expect('Content-Type', /json/)
             .then(res => {
                 expect(res.body).to.be.an('array')
-                //check atual sort is correct
+                expect(res.body.slice(0, 4)).to.deep.equal([
+                    {
+                        "App": "Hello Kitty Nail Salon",
+                        "Category": "GAME",
+                        "Rating": 4.2,
+                        "Reviews": "369203",
+                        "Size": "24M",
+                        "Installs": "50,000,000+",
+                        "Type": "Free",
+                        "Price": "0",
+                        "Content Rating": "Everyone",
+                        "Genres": "Casual;Pretend Play",
+                        "Last Updated": "April 17, 2018",
+                        "Current Ver": "1.5",
+                        "Android Ver": "4.1 and up"
+                    },
+                    {
+                        "App": "Helix Jump",
+                        "Category": "GAME",
+                        "Rating": 4.2,
+                        "Reviews": "1497361",
+                        "Size": "33M",
+                        "Installs": "100,000,000+",
+                        "Type": "Free",
+                        "Price": "0",
+                        "Content Rating": "Everyone",
+                        "Genres": "Action",
+                        "Last Updated": "April 9, 2018",
+                        "Current Ver": "1.0.6",
+                        "Android Ver": "4.1 and up"
+                    },
+                    {
+                        "App": "Block Puzzle Classic Legend !",
+                        "Category": "GAME",
+                        "Rating": 4.2,
+                        "Reviews": "17039",
+                        "Size": "4.9M",
+                        "Installs": "5,000,000+",
+                        "Type": "Free",
+                        "Price": "0",
+                        "Content Rating": "Everyone",
+                        "Genres": "Puzzle",
+                        "Last Updated": "April 13, 2018",
+                        "Current Ver": "2.9",
+                        "Android Ver": "2.3.3 and up"
+                    },
+                    {
+                        "App": "Temple Run 2",
+                        "Category": "GAME",
+                        "Rating": 4.3,
+                        "Reviews": "8118609",
+                        "Size": "62M",
+                        "Installs": "500,000,000+",
+                        "Type": "Free",
+                        "Price": "0",
+                        "Content Rating": "Everyone",
+                        "Genres": "Action",
+                        "Last Updated": "July 5, 2018",
+                        "Current Ver": "1.49.1",
+                        "Android Ver": "4.0 and up"
+                    }
+                ])
             })
     })
     //filter by wrong genre parameters fails
     it('should only filter if correct genre parameters', () => {
         return supertest(app)
             .get('/apps')
-            .query(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'])
+            .query({genre: 'other'})
             .expect(400, 'Genre can be Action, Puzzle, Strategy, Casual, Arcade, or Card.')
     })
     //filter by genre works with correct parameters
@@ -145,11 +206,3 @@ describe('GET /apps', () => {
     })
 
 })
-
-
-
-
-
-////sort in app.js
-////writing sort test
-////tests for wrong parameters - correct syntax?
